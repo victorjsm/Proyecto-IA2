c74527a7d220eae28aeaabfbbe2a916fec798e85
Carlo Polisano <carlo.polisano@gmail.com>
2015-06-02 21:36:34 -0430

    Comentarios y eso

--- mainNegamax.cc
+++ mainNegamax.cc
@@ -15,10 +15,19 @@
  *  Blai Bonet, bonet@ldc.usb.ve
  *
  *  Last revision: 11/08/2012
+ *  
+ *  Carlo Polisano
+ *  
+ *  Compile as such
+ *      g++ mainNegamax.cc -o negamax
+ *  and run 
+ *      ./negamax
  *
+ *  if negamax is not executable do
+ *      chmod -x negamax
  */
 
-#include "othello_cut.h" // won't work correctly until .h is fixed!
+#include "othello_cut.h" 
 #include <iostream>
 #include <limits>
 
@@ -42,37 +51,10 @@ int main(int argc, const char **argv) {
     
     cout << state;
     cout << "\nCall to negamax\n";
-    int score = negamaxAB(state,j-x,std::numeric_limits<int>::min(),std::numeric_limits<int>::max(),1);
+    int score = negamaxAB(state,31,std::numeric_limits<int>::min(),std::numeric_limits<int>::max(),1);
 
     cout << score;
     cout << "\n";
-    /*
-    for( int i = 0; PV[i] != -1; ++i ) {
-        bool player = i % 2 == 0; // black moves first!
-        int pos = PV[i];
-        cout << state;
-        cout << (player ? "Black" : "White")
-             << " moves at pos = " << pos << (pos == 36 ? " (pass)" : "")
-             << endl;
-        state = state.move(player, pos);
-        cout << "Board after " << i+1 << (i == 0 ? " ply:" : " plies:") << endl;
-    }
-    cout << state;
-    cout << "Value of the game = " << state.value() << endl;
-    cout << "#bits per state = " << sizeof(state) * 8 << endl;
-
-    if( argc > 1 ) {
-        int n = atoi(argv[1]);
-        cout << endl << "Apply " << n << " random movements at empty board:";
-        state = state_t();
-        for( int i = 0; i < n; ++i ) {
-            bool player = i % 2 == 0; // black moves first
-            int pos = state.get_random_move(player);
-            state = state.move(player, pos);
-            cout << " " << pos;
-        }
-        cout << endl << state;
-    */
 
     return 0;
 }
